501112cd249a4d4c6d53d94e083fa4ff51d2bb86
Pavel Anikin <PavelDidko@gmail.com>
2025-01-30 17:35:13 +0300

    update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,14 @@
+from brain_games.cli import welcome_user
+
 def brain_games():
     print('Welcome to the Brain Games!')
 
 
+  
+
 def main():
     brain_games()
-
+    welcome_user()    
 
 if __name__ == "__main__":
     main()
