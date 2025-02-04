c336c6961a45058eb9bd34b59022a5474d843233
Pavel Anikin <PavelDidko@gmail.com>
2025-02-04 21:05:38 +0300

    update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,11 @@
-from brain_games.scripts.cli1 import welcome_user
-from brain_games.scripts.cli1 import rendom_number
-from brain_games.scripts.cli1 import operator
-from brain_games.scripts.cli1 import name
+from cli1 import welcome_user
+from cli1 import rendom_number
+from cli1 import operator
+from cli1 import name
 import sys
 
 def brain_calc():
-    
+    welcome_user()
     print('What is the result of the expression?')
     answer = ''
     i = 0
@@ -16,16 +16,17 @@ def brain_calc():
         print(f'Question: {x} {oper} {y}')
         print('Your answer: ', end='')
         answer = input()
-        if f'{x} {oper} {y}' == answer:
+        result = eval(f"{x} {oper} {y}")
+
+        if result == answer:
             print('Correct!')
             i += 1
         else:
-            print(f''''{answer}' is wrong answer ;(. Correct answer was '175'. Let's try again, Sam!''')
+            print(f''''{answer}' is wrong answer ;(. Correct answer was {result}. Let's try again, {name}!''')
             sys.exit(0) 
     print(f'Congratulations, {name}!')
 
 def main():
-    welcome_user()
     brain_calc()    
 
 
