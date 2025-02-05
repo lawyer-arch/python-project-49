81dc79dd925f0fed16ee8ced73e2e08d2b681ba2
Pavel Anikin <PavelDidko@gmail.com>
2025-02-05 15:11:19 +0300

    add brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,34 @@
+from brain_games.cli1 import welcome_user, random_number
+import sys
+import math
+
+
+
+def brain_gcd():
+    name = welcome_user()  # Сохраняем имя
+    print('Find the greatest common divisor of given numbers.')
+    
+    i = 0
+    while i < 3:
+        x = random_number()
+        y = random_number()
+                
+        print(f'Question: {x} {y}')
+        answer = input('Your answer: ')
+        
+        result = math.gcd(x, y)  # Вычисляем правильный ответ
+        
+        if result == int(answer):  # Приводим ответ пользователя к числу
+            print('Correct!')
+            i += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was {result}.\n Let's try again, {name}!")
+            sys.exit(0)
+    
+    print(f'Congratulations, {name}!')
+
+def main():
+    brain_gcd()    
+
+if __name__ == "__main__":
+    main()
