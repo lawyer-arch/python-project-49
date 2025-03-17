91d7162befcc72969e160124c546aa5b30a5d4ec
Pavel Anikin <PavelDidko@gmail.com>
2025-03-17 13:46:22 +0300

    updata all fils

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,6 +3,7 @@ from brain_games.utils import welcome_user, conclusion_congratulations
 
 NUM_OF_ROUNDS = 3
 
+
 def c_brain_games(rules_of_the_game, game_logic_func):
     name = welcome_user()  # Сохраняем имя пользователя
     print(rules_of_the_game)
