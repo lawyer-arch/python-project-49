9aac52680fa9f5acb4b2aef35deced8173c0043e
Pavel Anikin <PavelDidko@gmail.com>
2025-04-01 16:11:33 +0300

    fixing syntax errors

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,6 +3,7 @@ import prompt
 
 NUM_OF_ROUNDS = 3
 
+
 def user_response():
     answer_user = input('Your answer: ')
     return answer_user.lower()
@@ -18,8 +19,6 @@ def welcome_user():
 def c_brain_games(rules_of_the_game, game_logic_func):
     name = welcome_user()
     print(rules_of_the_game)
-    
-
     for i in range(NUM_OF_ROUNDS):
         question, correct_answer = game_logic_func()
         print(f'Question: {question}')
@@ -29,8 +28,9 @@ def c_brain_games(rules_of_the_game, game_logic_func):
             print('Correct!')
                        
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was {correct_answer}. "
-            f"Let's try again, {name}!")
-            return
+            print(f'{answer} is wrong answer ;(. ' 
+                  f'Correct answer was {correct_answer}. '
+                  f'Let\'s try again, {name}!'
+            )
         
     print(f'Congratulations, {name}!')
